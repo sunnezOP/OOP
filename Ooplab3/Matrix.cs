4647a0014b928ce5caf2edd0d882edf1fa0f6bd4
sunnezOP <56665398+sunnezOP@users.noreply.github.com>
2020-03-19 08:49:57 +0200

    lab3
    lab3

---
+++ Ooplab3/Matrix.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nezhevets
+{
+    class Matrix
+    {
+        char[][] array;
+        int vowels;
+
+        public Matrix(char[][] arr)
+        {
+            array = arr;
+        }
+
+        public char[] this[int index]
+        {
+            get
+            {
+                if (index == 1)
+                {
+                    char[] diag = new char[array.Length];
+                    int k = -1;
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        for (int j = 0; j < array.Length; j++)
+                        {
+                            if (i == j) diag[++k] = array[i][j];
+                        }
+                    }
+                    return diag;
+                }
+                else
+                {
+                    char[] diag = new char[array.Length];
+                    int k = -1;
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        for (int j = 0; j < array.Length; j++)
+                        {
+                            if (i == j) diag[++k] = array[j][i];
+                        }
+                    }
+                    return diag;
+                }
+            }
+        }
+        public int Vowels
+        {
+            get
+            {
+                vowels = VowelsCount();
+                return vowels;
+            }
+        }
+        int VowelsCount()
+        {
+            int number = 0;
+            char[] vowels = { 'a', 'e', 'i', 'u', 'o', 'y' };
+            Dictionary<char, int> dict = new Dictionary<char, int>(vowels.Length);
+            foreach (char ch in vowels)
+                dict.Add(ch, 0);
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                foreach (char ch in array[i])
+                {
+                    if (dict.ContainsKey(ch))
+                        number++;
+                }
+            }
+            return number;
+        }
+    }
+}
---
+++ Ooplab3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Nezhevets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[][] arr = new char[][]
+            {
+                     new char[] {'a','b','c','d'},
+                     new char[] {'e','f','g','h'},
+            };
+
+            var matr = new Matrix(arr);
+
+            Console.WriteLine(matr.Vowels);
+        }
+    }
+}
